3abd7a277c43aca573498c7c9daeec5cfce3c8fd
Tomáš Tomek <tomastomek@seznam.cz>
2020-01-30 22:44:26 +0100

    Click to move (raycast hit with terrain collider)

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,15 +8,31 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    NavMeshAgent navMeshAgent;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<NavMeshAgent>().SetDestination(target.position);
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            navMeshAgent.SetDestination(hit.point);
+        }
     }
 }
