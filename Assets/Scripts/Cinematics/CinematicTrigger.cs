6c6bae7fba77b171a0ba375a01732d08611822a5
Tomáš Tomek <tomastomek@seznam.cz>
2020-02-11 23:22:04 +0100

    Trigger cinemachine cut scene

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Playables;
+using UnityEngine;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool alreadyTriggered = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                alreadyTriggered = true;
+            }
+            
+        }
+    }
+}
