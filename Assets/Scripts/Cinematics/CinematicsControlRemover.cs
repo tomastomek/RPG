6e3763d2ecec6f785dd4f5742d77c61fdebc850d
Tomáš Tomek <tomastomek@seznam.cz>
2020-02-17 22:16:04 +0100

    Player input in cutscenes

--- Assets/Scripts/Cinematics/CinematicsControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using RPG.Control;
+using RPG.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -7,11 +9,13 @@ namespace RPG.Cinematics
 {
     public class CinematicsControlRemover : MonoBehaviour
     {
+        private GameObject player;
         // Start is called before the first frame update
         void Start()
         {
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
+            GameObject player = GameObject.FindWithTag("Player");
         }
 
         // Update is called once per frame
@@ -21,12 +25,13 @@ namespace RPG.Cinematics
         }
         void DisableControl(PlayableDirector playableDirector)
         {
-
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector playableDirector)
         {
-
+            player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -62,6 +62,7 @@ namespace RPG.Combat
         {
             StopAttack();
             target = null;
+            mover.Cancel();
         }
 
         private void StopAttack()
