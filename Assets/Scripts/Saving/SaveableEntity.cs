2d3ca5d6c394935d53dc925e65fcef2a446d7040
Tomáš Tomek <tomastomek@seznam.cz>
2020-03-06 21:09:31 +0100

    Editing SerializedFields

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace RPG.Saving
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField] string uniqueIdentifier = default;
+
         public string GetUniqueIdentifier()
         {
-            
+            return uniqueIdentifier;
         }
 
         public object CaptureState()
@@ -19,6 +23,19 @@ namespace RPG.Saving
         public void RestoreState(object state)
         {
             Dictionary<string, object> stateDictionary = state as Dictionary<string, object>;
+        }
+
+        private void Update()
+        {
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty serializedProperty = serializedObject.FindProperty("uniqueIdentifier");
+
+            if (string.IsNullOrEmpty(serializedProperty.stringValue))
+            {
+                serializedProperty.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
         }
     }
 }
