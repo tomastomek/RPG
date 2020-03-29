bbf81b8be6187bc3c43c4107fa9c604f8d21065f
Tomáš Tomek <tomastomek@seznam.cz>
2020-03-29 16:50:03 +0200

    Damage from projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,6 +6,7 @@ public class Projectile : MonoBehaviour
     [SerializeField] float speed = 1;
 
     Health target = null;
+    float damage = 0;
 
     // Update is called once per frame
     void Update()
@@ -15,9 +16,10 @@ public class Projectile : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
-    public void SetTarget(Health target)
+    public void SetTarget(Health target, float damage)
     {
         this.target = target;
+        this.damage = damage;
     }
 
     private Vector3 GetAimLocation()
@@ -28,5 +30,12 @@ public class Projectile : MonoBehaviour
             return target.transform.position;
         }
         return target.transform.position + Vector3.up * targetCapsule.height / 2;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>() != target) { return; }
+        target.TakeDamage(damage);
+        Destroy(gameObject);
     }
 }
