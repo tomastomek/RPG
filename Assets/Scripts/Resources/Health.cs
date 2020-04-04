4fc0514804dddd306b5ea84b8cc08f7028542aa5
Tomáš Tomek <tomastomek@seznam.cz>
2020-04-04 13:13:26 +0200

    Bug fix - health race condition

--- Assets/Scripts/Resources/Health.cs
+++ Assets/Scripts/Resources/Health.cs
@@ -7,13 +7,16 @@ namespace RPG.Resources
 {
     public class Health : MonoBehaviour, ISaveable
     {
-        [SerializeField] float healthPoints = 100f;
+        float healthPoints = -1f;
 
         bool isDead = false;
 
         private void Start()
         {
-            healthPoints = GetComponent<BaseStats>().GetStat(Stat.Health);
+            if (healthPoints < 0)
+            {
+                healthPoints = GetComponent<BaseStats>().GetStat(Stat.Health);
+            }
         }
 
         public bool IsDead()
@@ -66,8 +69,9 @@ namespace RPG.Resources
                 healthPoints = health;
             }
             else
-            {
+            {                
                 Die();
+                isDead = true;
             }
         }
     }
