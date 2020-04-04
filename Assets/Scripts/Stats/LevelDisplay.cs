1c4d52a3634e34b9e2257ed04acd152a33fdc7b7
Tomáš Tomek <tomastomek@seznam.cz>
2020-04-04 10:30:44 +0200

    Displaying level

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Stats
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        BaseStats baseStats;
+
+        void Awake()
+        {
+            baseStats = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseStats>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GetComponent<Text>().text = baseStats.GetLevel().ToString();
+        }
+    }
+}
